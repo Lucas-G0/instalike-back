56c0c1f39aca160d75fc4679830357a6864de6f7
Lucas Gonçalves <89371571+Lucas-G0@users.noreply.github.com>
2024-11-21 11:54:54 -0300

    feat: getting data from db

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import conectarBanco from './src/config/dbConfig.js';
+
+const conexao = await conectarBanco(process.env.STRING_CONEXAO);
 
 const app = express();
 app.use(express.json());
@@ -7,15 +10,23 @@ app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-});
+async function getAllLivros() {
+    const db = conexao.db("imersaodev");
+    const colecao = db.collection("livros");
+    return colecao.find().toArray();
+}
+
 
 function buscarLivroPorID(id){
     return livros.findIndex((livro) => {
         return livro.id === Number(id);
     });
 };
+
+app.get("/livros", async (req, res) => {
+    const livros = await getAllLivros();
+    res.status(200).json(livros);
+});
 
 app.get("/livros/:id", (req, res) => {
     const index = buscarLivroPorID(req.params.id);
