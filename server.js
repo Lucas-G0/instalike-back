2b040cb595a832309dd36e45480e0ecaaaf0a924
Lucas Gonçalves <89371571+Lucas-G0@users.noreply.github.com>
2024-11-21 13:59:13 -0300

    fix: added router to server.js

--- server.js
+++ server.js
@@ -1,34 +1,10 @@
 import express from 'express';
-import conectarBanco from './src/config/dbConfig.js';
-
-const conexao = await conectarBanco(process.env.STRING_CONEXAO);
+import router from './src/routes/livrosRoutes.js';
 
 const app = express();
-app.use(express.json());
+router(app);
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-async function getAllLivros() {
-    const db = conexao.db("imersaodev");
-    const colecao = db.collection("livros");
-    return colecao.find().toArray();
-}
-
-
-function buscarLivroPorID(id){
-    return livros.findIndex((livro) => {
-        return livro.id === Number(id);
-    });
-};
-
-app.get("/livros", async (req, res) => {
-    const livros = await getAllLivros();
-    res.status(200).json(livros);
-});
-
-app.get("/livros/:id", (req, res) => {
-    const index = buscarLivroPorID(req.params.id);
-    res.status(200).json(livros[index]);
-});
