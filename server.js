da72335a6841591d40f3a0859904f8f46f17feae
Lucas Gonçalves <89371571+Lucas-G0@users.noreply.github.com>
2024-11-21 10:16:42 -0300

    delete: removed const livros

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("API funcionando!");
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+function buscarLivroPorID(id){
+    return livros.findIndex((livro) => {
+        return livro.id === Number(id);
+    });
+};
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscarLivroPorID(req.params.id);
+    res.status(200).json(livros[index]);
 });
