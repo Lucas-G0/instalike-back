88282a99c3cba6d01a452fbd2d0744d1116ef843
Lucas Gonçalves <89371571+Lucas-G0@users.noreply.github.com>
2024-11-22 10:54:43 -0300

    feat: create uploadFile function

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import {getAllLivros, createLivro} from '../models/livrosModel.js';
 
 export async function listarLivros(req, res) 
@@ -12,6 +13,28 @@ export async function createNewLivro(req, res)
     try {
         const livro = await createLivro(novoLivro);
         res.status(201).json(livro);
+    } catch (erro){
+        console.error(erro.message);
+        res.status(500).json({"Erro:":" Houve um erro ao tentar criar um novo livro."});
+    }
+}
+
+export async function uploadFile(req, res)
+{
+    const novoLivro = {
+        titulo: "",
+        autor: "",
+        ano: "",
+        genero: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    }
+
+    try {
+        const livroCriado = await createLivro(novoLivro);
+        const imagemAtualizada = `uploads/${livroCriado.insertedId}.jpeg`;
+        fs.renameSync(req.file.path, imagemAtualizada);
+        res.status(201).json(livroCriado);
     } catch (erro){
         console.error(erro.message);
         res.status(500).json({"Erro:":" Houve um erro ao tentar criar um novo livro."});
