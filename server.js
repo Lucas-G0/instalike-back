38f1c1361bb2c50be15fb32d97bfb34394008714
Lucas Gonçalves <89371571+Lucas-G0@users.noreply.github.com>
2024-11-25 08:20:23 -0300

    feat: defined route uploads for images

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './src/routes/livrosRoutes.js';
 
 const app = express();
+app.use(express.static("uploads"));
 router(app);
 
 app.listen(3000, () => {
