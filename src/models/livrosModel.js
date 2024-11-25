0894bc8bec0a266325e8ae7965deb7ba59510c2a
Lucas Gonçalves <89371571+Lucas-G0@users.noreply.github.com>
2024-11-25 08:19:58 -0300

    fix: get livro by id model

--- src/models/livrosModel.js
+++ src/models/livrosModel.js
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb';
 import conectarBanco from '../config/dbConfig.js';
 
 const conexao = await conectarBanco(process.env.STRING_CONEXAO);
@@ -13,3 +14,18 @@ export async function createLivro(livro) {
     const colecao = db.collection("livros");
     return colecao.insertOne(livro);
 }
+
+export async function updateLivro(id, novoLivro) {
+    const db = conexao.db("imersaodev");
+    const colecao = db.collection("livros");
+    const objID = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set: novoLivro});
+}
+
+export async function buscarLivroPorID(id)
+{
+    const db = conexao.db("imersaodev");
+    const colecao = db.collection("livros");
+    const livro = colecao.findOne({_id: new ObjectId(id)});
+    return livro;
+};
